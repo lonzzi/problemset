0d4e6c4e5d226420638973e39f49c123170f80e4
lonzzi <lonzzi@qq.com>
2022-03-19 22:05:45 +0800

    daily

---
+++ leetcode/golang/problems/606. 根据二叉树创建字符串.go
@@ -0,0 +1,44 @@
+package problems
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 低效率版本
+// func Tree2str(root *TreeNode) string {
+// 	ans := ""
+//     var preorder func(*TreeNode)
+//     preorder = func(root *TreeNode) {
+//         if root != nil {
+//             ans += fmt.Sprintf("(%v", root.Val)
+//             if root.Left == nil && root.Right != nil {
+//                 ans += "()"
+//             }
+//             preorder(root.Left)
+//             preorder(root.Right)
+//             ans += ")"
+//         }
+//     }
+//     preorder(root)
+//     return ans[1:len(ans)-1]
+// }
+
+func Tree2str(root *TreeNode) string {
+	switch {
+	case root == nil:
+		return ""
+	case root.Left == nil && root.Right == nil:
+		return strconv.Itoa(root.Val)
+	case root.Right == nil:
+		return fmt.Sprintf("%s(%s)", strconv.Itoa(root.Val), Tree2str(root.Left))
+	default:
+		return fmt.Sprintf("%s(%s)(%s)", strconv.Itoa(root.Val), Tree2str(root.Left), Tree2str(root.Right))
+	}
+}
